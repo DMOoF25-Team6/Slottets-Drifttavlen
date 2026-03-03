44a3d7e1005a9207ea80678581b1998a27db1d37
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2026-03-04 07:21:15 +0800

    Add repository

---
+++ src/Infrastructure/Repositories/IResidentNoteRepository.cs
@@ -0,0 +1,12 @@
+// Copyright (c) 2026 Team6. All rights reserved. 
+//  No warranty, explicit or implicit, provided.
+
+using Core.Interfaces.Repositories;
+
+using Domain.Entities;
+
+namespace Infrastructure.Repositories;
+
+public class ResidentNotesRepository : Repository<ResidentNote>, IResidentNoteRepository
+{
+}
