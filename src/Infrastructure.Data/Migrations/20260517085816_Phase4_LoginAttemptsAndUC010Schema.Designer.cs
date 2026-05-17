7d2d679d479fc2030c7fcb5175557250ab1b811a
codemikemike <mikr71394@edu.ucl.dk>
2026-05-17 10:58:25 +0200

    Add Phase4 migration: LoginAttempts table

--- src/Infrastructure.Data/Migrations/20260517085816_Phase4_LoginAttemptsAndUC010Schema.Designer.cs
+++ src/Infrastructure.Data/Migrations/20260517085816_Phase4_LoginAttemptsAndUC010Schema.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Data.Persistent;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260517085816_Phase4_LoginAttemptsAndUC010Schema")]
+    partial class Phase4_LoginAttemptsAndUC010Schema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
