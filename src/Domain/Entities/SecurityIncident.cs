8575d2467a97538574222c56607b27738b76034b
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2026-05-21 13:53:54 +0800

    Add using System.ComponentModel.DataAnnotations.Schema

--- src/Domain/Entities/SecurityIncident.cs
+++ src/Domain/Entities/SecurityIncident.cs
@@ -2,6 +2,7 @@
 //  No warranty, explicit or implicit, provided.
 
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 using Domain.Enums;
 using Domain.Interfaces;
